2fc814ecf5dcf91c3d3af37375bb7ed321d3e1d5
Jakub Korál <jakub.koral@apiary.io>
2013-05-21 11:58:29 +0200

    Convert to tabs, add some comments into bulder

--- tools/prism-node-builder.js
+++ tools/prism-node-builder.js
@@ -1,87 +1,95 @@
 // Load dependencies
 var fs = require('fs');
+
+// Load components
 var components = require('./../components.js');
 
-var contentLangs = [];
-var insertedLang = {};
-var toBeInserted = [];
-
 var files = [{
-  'langKey': 'core',
-  'require': false
+	'langKey': 'core'
 }];
 
+// add language definition from components
 for (language in components.languages) {
-  if (language && language !== 'meta' && components.languages[language]) {
-    files.push({
-      'langKey': (language + ''),
-      'require': (components.languages[language]['require'] || false)
-    });
-  }
+	if (language && language !== 'meta' && components.languages[language]) {
+		files.push({
+			'langKey': language + '',
+			'require': components.languages[language]['require'] || false
+		});
+	}
 }
 
 var filesNum = files.length;
 
 var i = 0;
-var ii = 0;
+var totalRuns = 0;
 
-console.log('====== WHILE ======');
+var insertedLang = {};
+var toBeInserted = [];
 
+// Load files according to their requirements
+// Languages without require go first; if required already available, load the new lang too
 while (files.length) {
-  if (!files[i]) {
-    i = 0;
-  }
-  else if (!files[i].require || insertedLang[files[i].require]) {
-    var removed = files.splice(i, 1).pop();
-    insertedLang[removed['langKey']] = true;
-    toBeInserted.push(removed['langKey']);
-    i = 0;
-  }
-  else if (i >= filesNum || i >= files.length) {
-    i = 0;
-  }
-  else {
-    i++;
-  }
-  if (ii > 100) {
-    break;
-  }
-  ++ii;
+	if (!files[i]) {
+		i = 0;
+	}
+	else if (!files[i].require || insertedLang[files[i].require]) {
+		var found = files.splice(i, 1).pop();
+		insertedLang[found['langKey']] = true;
+		toBeInserted.push(found['langKey']);
+		i = 0;
+	}
+	else if (i >= filesNum || i >= files.length) {
+		i = 0;
+	}
+	else {
+		i++;
+	}
+	if (totalRuns > 100) {
+		// too many runs, exit
+		console.error('Too many runs. Are there all required files?');
+		console.log(JSON.stringify(files, null, '\t'));
+		console.error('Unhandled files list above:');
+		process.exit();
+		break;
+	}
+	totalRuns++;
 }
 
+// work with the toBeInserted files-list, already in the right order
+var loadedLangs    = [];
+var loadedLangsNum = 0;
 
-var handleError = function (error) {
-  console.log('An error occured.');
-  console.log(error);
+function handleError (error) {
+	console.log('An error occured.');
+	console.log(error);
+	process.exit();
 };
 
-var contentLangsNum = 0;
+function handleFile (fileNamePart) {
+	fs.readFile('./components/prism-' + fileNamePart + '.js', function(error, data) {
+		if (error) return finishAll(error);
 
-var handleFile = function (fileName) {
-  fs.readFile('./components/prism-' + fileName + '.js', function(error, data) {
-    if (error) return finishAll(error);
+		loadedLangs.push(data.toString('utf8'));
+		loadedLangsNum++;
 
-    contentLangs.push(data.toString('utf8'));
-    contentLangsNum++;
+		if (loadedLangsNum === filesNum) return finishAll(null);
 
-    if (contentLangsNum === filesNum) return finishAll(null);
+		handleFile(toBeInserted.shift());
 
-    handleFile(toBeInserted.shift());
-
-  });
+	});
 };
 
-finishAll = function (error) {
+function finishAll (error) {
 
-  if (error) return handleError(error);
+	if (error) return handleError(error);
 
-  var prismBuild = contentLangs.shift();
-  prismBuild = prismBuild.replace('/' + '*<Plugins>*' + '/', contentLangs.join('\n\n'));
+	var prismBuild = loadedLangs.shift();
+	prismBuild = prismBuild.replace('/' + '*<Plugins>*' + '/', loadedLangs.join('\n\n'));
 
-  fs.writeFile('./build/prism.js', prismBuild, 'utf8', function (error) {
-    if (error) return handleError(error);
-    console.log('Done.\n');
-  });
+	fs.writeFile('./build/prism.js', prismBuild, 'utf8', function (error) {
+		if (error) return handleError(error);
+		console.log('Done.\n');
+	});
 
 };
 
